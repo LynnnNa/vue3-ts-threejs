a29983b71bdcaa5d8c3812779670eb9f1cb9a26b
LynnnNa <1596525333@qq.com>
2023-05-05 11:21:42 +0800

    add router sass husky lint-staged eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-// import { resolve } from 'path';
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // resolve: {
-    //   alias :{  //这是一个对象吧！它得有键跟键名吧！
-    //    "@": path.resolve(__dirname, "src"),
-    //  }
-    ],
+	plugins: [
+		vue(),
+		eslintPlugin({
+			include: ["src/**/*.ts", "src/**/*.vue", "src/*.ts", "src/*.vue"]
+		})
+	],
+	resolve: {
+		alias: {
+			//这是一个对象吧！它得有键跟键名吧！
+			"@": resolve(__dirname, "src")
+		}
+	},
+	base: "./package", // 打包路径
+	server: {
+		port: 4000,
+		open: true, // 服务启动时自动打开浏览器
+		cors: true // 允许跨域
+	}
 })
